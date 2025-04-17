71e3bdef2777bd57e88a3cde796493dcf560d7cb
Alleexx129 <Alleexx129@gmail.com>
2025-04-16 22:19:06 -0400

    Update

---
+++ src/MainPage.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function MainPage() {
+  return (
+    <p>If you are not redirected automatically, <a href="/WorkspaceExample/">click here</a>.</p>
+  );
+}
+
+export default MainPage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
+import App2 from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+  </StrictMode>,
+)
+createRoot(document.getElementById('root2')).render(
+  <StrictMode>
+    <App2 />
   </StrictMode>,
 )
