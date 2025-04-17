2fc329ca09e369d514a457118b4d87f0303b9e1e
Alleexx129 <Alleexx129@gmail.com>
2025-04-16 22:20:54 -0400

    Update

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -2,7 +2,9 @@ import './App.css';
 
 function MainPage() {
   return (
+    <div className="MainPage">
     <p>If you are not redirected automatically, <a href="/WorkspaceExample/">click here</a>.</p>
+    </div>
   );
 }
 
