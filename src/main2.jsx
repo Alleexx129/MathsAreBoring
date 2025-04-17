0056869f7bb9db008943622b5cb657dc996d9f2b
Alleexx129 <Alleexx129@gmail.com>
2025-04-16 22:27:11 -0400

    Update

--- src/main2.jsx
+++ src/main2.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import App from './App.jsx'
+import App2 from './MainPage.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root2')).render(
   <StrictMode>
-    <App />
+    <App2 />
   </StrictMode>,
 )
