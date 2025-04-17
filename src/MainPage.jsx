0b5a362f9cc38d02152197d0857c4db73d45373c
Alleexx129 <Alleexx129@gmail.com>
2025-04-16 23:43:22 -0400

    Update

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
 import './App.css';
 
 function MainPage() {
   return (
-    <div className="MainPage">
-    <p>If you are not redirected automatically, <a href="https://alleexx129.github.io/MathsAreBoring/WorkspaceExample/">click here</a>.</p>
-    </div>
+    <>
+      <Router basename="/MathsAreBoring">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<App />} />
+        </Routes>
+      </Router>
+      <div className="MainPage">
+        <p>If you are not redirected automatically, <a href="https://alleexx129.github.io/MathsAreBoring/WorkspaceExample/">click here</a>.</p>
+      </div>
+    </>
   );
 }
 
