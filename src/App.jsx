9807178cc231bc25cefba182cca41a714dfd4ac2
Alleexx129 <Alleexx129@gmail.com>
2025-04-17 21:54:10 -0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,20 @@ import Draggable from 'react-draggable';
 import './App.css';
 
 function App() {
+  const [genBoxes] = useState([
+    { id: 69, x: 0.4 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxX", text: "X" },
+    { id: 696, x: 0.5 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxXX", text: "X²" },
+    { id: 6969, x: 0.6 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxY", text: "Y" },
+    { id: 69696, x: 0.7 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxYY", text: "Y²" },
+    
+    { id: 696969, x: 0.8 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxNegX", text: "-X" },
+    { id: 6969696, x: 0.9 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxNegXX", text: "-X²" },
+    { id: 69696969, x: 1 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxNegY", text: "-Y" },
+    { id: 696969696, x: 1.1 * visualViewport.width / 2, y: 0*visualViewport.height / 2, className: "BoxNegYY", text: "-Y²" }
+  ]);
   const [boxes, setBoxes] = useState([
-    { id: 1, x: 0, y: 0, className: "BoxX", text: "X" },
-    { id: 2, x: 120, y: 0, className: "BoxX", text: "X" },
-    { id: 3, x: 240, y: 0, className: "BoxY", text: "Y" },
-    { id: 4, x: 360, y: 0, className: "BoxY", text: "Y" }
   ]);
   const boxRefs = useRef({});
-
-  const addBoxX = () => {
-    const newId = Date.now();
-    setBoxes(prev => [...prev, { id: newId, x: Math.random() * window.innerWidth / 2, y: Math.random() * window.innerHeight / 2, className: "BoxX", text: `X` }]);
-  };
-  const addBoxY = () => {
-    const newId = Date.now();
-    setBoxes(prev => [...prev, { id: newId, x: Math.random() * window.innerWidth / 2, y: Math.random() * window.innerHeight / 2, className: "BoxY", text: `Y` }]);
-  };
-  const addBoxNegX = () => {
-    const newId = Date.now();
-    setBoxes(prev => [...prev, { id: newId, x: Math.random() * window.innerWidth / 2, y: Math.random() * window.innerHeight / 2, className: "BoxNegX", text: `-X` }]);
-  };
-  const addBoxNegY = () => {
-    const newId = Date.now();
-    setBoxes(prev => [...prev, { id: newId, x: Math.random() * window.innerWidth / 2, y: Math.random() * window.innerHeight / 2, className: "BoxNegY", text: `-Y` }]);
-  };
 
   const checkOverlap = (rect1, rect2) => {
     return !(
@@ -42,6 +32,23 @@ function App() {
     if (!currentBox) return;
 
     const currentRect = currentBox.current.getBoundingClientRect();
+    const uniqueId = Date.now() + Math.random() * 10;
+
+    for (let genBox of genBoxes) {
+      if (genBox.id === id) {
+        setBoxes(prev => [
+          ...prev,
+          {
+            id: uniqueId,
+            x: data.x,
+            y: data.y,
+            className: currentBox.current.className,
+            text: genBox.text
+          }
+        ]);
+        break;
+      }
+    }
 
     for (let other of boxes) {
       if (other.id === id) continue;
@@ -52,7 +59,7 @@ function App() {
       const otherRect = otherRef.current.getBoundingClientRect();
 
       if (checkOverlap(currentRect, otherRect) && !(currentBox.current.className.includes("BoxXX") && otherRef.current.className.includes("BoxXX")) && !(currentBox.current.className.includes("BoxYY") && otherRef.current.className.includes("BoxYY")) && otherRef.current.className.split(" ")[0] == currentBox.current.className.split(" ")[0]) {
-        if (currentBox.current.className.split(" ")[0] == "BoxX") {
+        /*if (currentBox.current.className.split(" ")[0] == "BoxX") {
           setBoxes(prev =>
             prev
               .filter(box => box.id !== other.id)
@@ -61,25 +68,7 @@ function App() {
                   ? { ...box, className: "BoxXX", text: "X²", x: data.x, y: data.y }
                   : box)
           );
-        } else if (currentBox.current.className.split(" ")[0] == "BoxY") {
-          setBoxes(prev =>
-            prev
-              .filter(box => box.id !== other.id)
-              .map(box =>
-                box.id === id
-                  ? { ...box, className: "BoxYY", text: "Y²", x: data.x, y: data.y }
-                  : box)
-          );
-        } else if (currentBox.current.className.split(" ")[0] == "BoxNegX") {
-          setBoxes(prev =>
-            prev
-              .filter(box => box.id !== other.id)
-              .map(box =>
-                box.id === id
-                  ? { ...box, className: "BoxNegXX", text: "-X²", x: data.x, y: data.y }
-                  : box)
-          );
-        } else if (currentBox.current.className.split(" ")[0] == "BoxNegY") {
+        }  else if (currentBox.current.className.split(" ")[0] == "BoxNegY") {
           setBoxes(prev =>
             prev
               .filter(box => box.id !== other.id)
@@ -88,20 +77,20 @@ function App() {
                   ? { ...box, className: "BoxNegYY", text: "-Y²", x: data.x, y: data.y }
                   : box)
           );
-        } 
+        } */
       } else if (checkOverlap(currentRect, otherRect) && (currentBox.current.className.includes("Neg") || otherRef.current.className.includes("Neg"))) {
         if (otherRef.current.className.split(" ")[0].replace("Neg", "") == currentBox.current.className.split(" ")[0] || currentBox.current.className.split(" ")[0].replace("Neg", "") == otherRef.current.className.split(" ")[0]) {
+          
           setBoxes(prev =>
-            prev.filter(box => box.id !== other.id && box.id !== currentBox.current.id)
+            prev.filter(box => box.id !== other.id && box.id !== currentBox.current.id && box.id !== uniqueId)
             .map(box =>
               box.id === id
                 ? { ...box, className: "", text: "", x: 0, y: 0 }
                 : box)
           );
         }
-      }
+      } 
     }
-
     setBoxes(prev =>
       prev.map(b => (b.id === id ? { ...b, x: data.x, y: data.y } : b))
     );
@@ -109,30 +98,46 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={addBoxX}>Add X</button>
-      <button onClick={addBoxY}>Add Y</button>
-      <button onClick={addBoxNegX}>Add -X</button>
-      <button onClick={addBoxNegY}>Add -Y</button>
-
-      {boxes.map(box => {
-        if (!boxRefs.current[box.id]) {
-          boxRefs.current[box.id] = React.createRef();
-        }
-
-        return (
-          <Draggable
-            key={box.id}
-            nodeRef={boxRefs.current[box.id]}
-            position={{ x: box.x, y: box.y }}
-            onStop={(_, data) => onStop(box.id, data)}>
-            <div ref={boxRefs.current[box.id]} className={box.className}>
-              {box.text}
-            </div>
-          </Draggable>
-        );
-      })}
+  
+      {[
+        ...boxes.map(box => {
+          if (!boxRefs.current[box.id]) {
+            boxRefs.current[box.id] = React.createRef();
+          }
+  
+          return (
+            <Draggable
+              key={box.id}
+              nodeRef={boxRefs.current[box.id]}
+              position={{ x: box.x, y: box.y }}
+              onStop={(_, data) => onStop(box.id, data)}>
+              <div ref={boxRefs.current[box.id]} className={box.className}>
+                {box.text}
+              </div>
+            </Draggable>
+          );
+        }),
+  
+        ...genBoxes.map(box => {
+          if (!boxRefs.current[box.id]) {
+            boxRefs.current[box.id] = React.createRef();
+          }
+  
+          return (
+            <Draggable
+              key={box.id}
+              nodeRef={boxRefs.current[box.id]}
+              position={{ x: box.x, y: box.y }}
+              onStop={(_, data) => onStop(box.id, data)}>
+              <div ref={boxRefs.current[box.id]} className={box.className}>
+                {box.text}
+              </div>
+            </Draggable>
+          );
+        })
+      ]}
     </div>
-  );
+  );  
 }
 
 export default App;
